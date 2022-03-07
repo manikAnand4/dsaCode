f2f6928b684eda86d6777bda1dba974b5df16683
manikAnand4 <89723594+manikAnand4@users.noreply.github.com>
2022-03-07 23:35:40 +0530

    Update rodCutting.cpp

--- rodCutting.cpp
+++ rodCutting.cpp
@@ -14,11 +14,28 @@ int getAns(vector<int>&l, vector<int>&p, int n, vector<int>&dp){
 	}
 	return dp[n]=mans;
 }
+
+int getAnsBU(vector<int>&l, vector<int>&p, int n){
+	int dp[n+1];
+	dp[0]=0;
+	for(int i=1;i<=n;i++){
+		int ans=INT_MIN;
+		for(int j=0;j<i;j++){
+			int cut=j+1;
+			int cans=p[j]+dp[i-cut];
+			ans=max(cans,ans);
+		}
+		dp[i]=ans;
+	}
+	return dp[n];
+}
+
 int main(){
 	int n=8;
 	vector<int>length{1,2,3,4,5,6,7,8};
-	vector<int>price{3,5,8,9,10,17,17,20};
+	vector<int>price{1,5,8,9,10,17,17,20};
 	vector<int>dp(n+1,-1);
-	int ans=getAns(length, price, n, dp);
+	// int ans=getAns(length, price, n, dp);
+	int ans=getAnsBU(length, price, n);
 	cout<<ans<<'\n';
 }
